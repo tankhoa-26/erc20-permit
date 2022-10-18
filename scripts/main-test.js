5b291c66d7b75546d51a507b334ed0e737caf80b
Trinh Tan Khoa <ttkhoa.hcmus@gmail.com>
2022-10-18 09:43:43 +0700

    Rename and update test

--- scripts/main-test.js
+++ scripts/main-test.js
@@ -1,10 +1,3 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-// const hre = require("hardhat");
 const {ethers}  = require("hardhat");
 
 const _DECIMAL = ethers.BigNumber.from(10).pow(18);
@@ -74,7 +67,7 @@ async function main() {
     const accounts = await ethers.getSigners();
     const deployer = accounts[0];
 
-    //DAL Token
+    //Deploy DAL Token
     const TokenPermitFactory = await ethers.getContractFactory("TokenPermit", deployer);
     const DALToken = await TokenPermitFactory.deploy();
     await DALToken.deployed();
@@ -83,11 +76,12 @@ async function main() {
     console.log("Mint 1000 DAL token to deployer");
     await DALToken.mint(deployer.address, amountMinted);
 
-    //Vault contract
+    //Deploy Vault contract
     const VaultFactory = await ethers.getContractFactory("Vault", deployer);
     const vault = await VaultFactory.deploy(DALToken.address);
     console.log("Deploy Vault contract success");
 
+    //Mimic the step that the user signs his message
     const {v, r, s} = await getPermitSignature(deployer, DALToken, vault.address, fee, deadline);
 
     console.log("v: ", v);
@@ -100,9 +94,6 @@ async function main() {
 }
 
 
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
